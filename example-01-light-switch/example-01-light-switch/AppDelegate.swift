c51ad14c2f187dcee2f53689aab4be65d9517b5d
Klemen Verdnik <klemen.verdnik@gmail.com>
2015-12-13 21:49:10 -0800

    Setting the stage for the XCode example project.

---
+++ example-01-light-switch/example-01-light-switch/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  example-01-light-switch
+//
+//  Created by Klemen Verdnik on 12/10/15.
+//  Copyright © 2015 Klemen Verdnik. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ example-01-light-switch/example-01-light-switchTests/example_01_light_switchTests.swift
@@ -0,0 +1,36 @@
+//
+//  example_01_light_switchTests.swift
+//  example-01-light-switchTests
+//
+//  Created by Klemen Verdnik on 12/10/15.
+//  Copyright © 2015 Klemen Verdnik. All rights reserved.
+//
+
+import XCTest
+@testable import example_01_light_switch
+
+class example_01_light_switchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
